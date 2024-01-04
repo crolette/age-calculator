a58ad54d8d10520f36e9388e5df32d0397db9eda
Crolweb <crolweb@gmail.com>
2024-01-04 15:40:17 +0100

    date calculation js

--- README-template.md
+++ README-template.md
@@ -14,9 +14,6 @@ This is a solution to the [Age calculator app challenge on Frontend Mentor](http
   - [Continued development](#continued-development)
   - [Useful resources](#useful-resources)
 - [Author](#author)
-- [Acknowledgments](#acknowledgments)
-
-**Note: Delete this note and update the table of contents based on what sections you keep.**
 
 ## Overview
 
@@ -33,19 +30,11 @@ Users should be able to:
   - The date is invalid e.g. 31/04/1991 (there are 30 days in April)
 - View the optimal layout for the interface depending on their device's screen size
 - See hover and focus states for all interactive elements on the page
-- **Bonus**: See the age numbers animate to their final number when the form is submitted
+- [TODO] **Bonus**: See the age numbers animate to their final number when the form is submitted
 
 ### Screenshot
 
-![](./screenshot.jpg)
-
-Add a screenshot of your solution. The easiest way to do this is to use Firefox to view your project, right-click the page and select "Take a Screenshot". You can choose either a full-height screenshot or a cropped one based on how long the page is. If it's very long, it might be best to crop it.
-
-Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it.
-
-Then crop/optimize/edit your image however you like, add it to your project, and update the file path in the image above.
-
-**Note: Delete this note and the paragraphs above when you add your screenshot. If you prefer not to add a screenshot, feel free to remove this entire section.**
+![](./images/screenshot.png)
 
 ### Links
 
@@ -59,43 +48,13 @@ Then crop/optimize/edit your image however you like, add it to your project, and
 - Semantic HTML5 markup
 - CSS custom properties
 - Flexbox
-- CSS Grid
-- Mobile-first workflow
-- [React](https://reactjs.org/) - JS library
-- [Next.js](https://nextjs.org/) - React framework
-- [Styled Components](https://styled-components.com/) - For styles
-
-**Note: These are just examples. Delete this note and replace the list above with your own choices**
+- JS
 
 ### What I learned
 
-Modify color of an SVG file
-
-outline for focus elements
-
-Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
-
-To see how you can add code snippets, see below:
-
-```html
-<h1>Some HTML code I'm proud of</h1>
-```
-
-```css
-.proud-of-this-css {
-  color: papayawhip;
-}
-```
-
-```js
-const proudOfThisFunc = () => {
-  console.log("🎉");
-};
-```
-
-If you want more help with writing markdown, we'd recommend checking out [The Markdown Guide](https://www.markdownguide.org/) to learn more.
-
-**Note: Delete this note and the content within this section and replace with your own learnings.**
+- Styling form elements
+- Handle dates
+- Get deeper in JS
 
 ### Continued development
 
@@ -111,9 +70,3 @@ Styling forms is quite challenging as there are, i.e. default tooltips that cann
 - Website - [Crolweb](https://www.crolweb.be)
 - Github - [Crolweb](https://github.com/crolette)
 - Frontend Mentor - [@crolette](https://www.frontendmentor.io/profile/crolette)
-
-## Acknowledgments
-
-This is where you can give a hat tip to anyone who helped you out on this project. Perhaps you worked in a team or got some inspiration from someone else's solution. This is the perfect place to give them some credit.
-
-**Note: Delete this note and edit this section's content as necessary. If you completed this challenge by yourself, feel free to delete this section entirely.**
--- app.js
+++ app.js
@@ -1,30 +1,129 @@
 const form = document.getElementById("formDate");
-console.log(form);
 const inputDay = document.getElementById("day");
-console.log(inputDay.validity);
 const inputMonth = document.getElementById("month");
-console.log(inputMonth.validity);
 const inputYear = document.getElementById("year");
-console.log(inputYear.validity);
+let spanDays = document.getElementById("result__days");
+let spanMonths = document.getElementById("result__months");
+let spanYears = document.getElementById("result__years");
+const YEARMS = 31556952000;
+const MONTHMS = 2629746000;
+const DAYMS = 86400000;
+let error = 0;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("submit");
-  checkMissingValue(inputDay);
-  checkMissingValue(inputMonth);
-  checkMissingValue(inputYear);
-
-  checkValueRange(inputDay);
+  removeErrorClass();
+  cleanDifference();
+  error = 0;
+  checkMissingValue(inputDay, "a valid day");
+  checkMissingValue(inputMonth, "a valid month");
+  checkMissingValue(inputYear, "in the past");
+  dateValidity();
+  if (error == 0) {
+    dateUser(inputDay.value, inputMonth.value, inputYear.value);
+  }
 });
+
+function cleanDifference() {
+  spanDays.innerText = "--";
+  spanMonths.innerText = "--";
+  spanYears.innerText = "--";
+}
+
+function removeErrorClass() {
+  inputDay.parentElement.classList.remove("error");
+  inputMonth.parentElement.classList.remove("error");
+  inputYear.parentElement.classList.remove("error");
+}
+
+function addErrorClass() {
+  inputDay.parentElement.classList.add("error");
+  inputMonth.parentElement.classList.add("error");
+  inputYear.parentElement.classList.add("error");
+}
 
 // Check if the input has a value or not
 // @param: input = input element (day, month, year)
-function checkMissingValue(input) {
+function checkMissingValue(input, text) {
   if (input.validity.valueMissing) {
     input.nextElementSibling.innerText = "This field is required";
-    input.parentElement.classList.add("error");
+    error = 1;
+  } else if (input.validity.rangeOverflow || input.validity.rangeUnderflow) {
+    input.nextElementSibling.innerText = "Must be " + `${text}`;
+    error = 1;
   } else {
-    input.parentElement.classList.remove("error");
     input.nextElementSibling.innerText = "";
+  }
+
+  errorClass(input);
+}
+
+// Change the class if the input is valid or not
+// @param: input = input element (day, month, year)
+function errorClass(input) {
+  if (input.validity.valid) {
+    input.parentElement.classList.remove("error");
+  } else {
+    input.parentElement.classList.add("error");
+  }
+}
+
+// Create date with user input
+// @day
+
+function dateUser(day, month, year) {
+  const date = new Date();
+  date.setMonth(month - 1, day);
+  date.setFullYear(year);
+  compareDate(date);
+}
+
+function compareDate(dateUser) {
+  const today = new Date();
+
+  let dateDifference = today - dateUser;
+  if (dateDifference < 0) {
+    addErrorClass();
+    inputYear.nextElementSibling.innerText = "Must be in the past";
+  } else {
+    renderDateDifference(dateDifference);
+  }
+}
+
+function renderDateDifference(dateDifference) {
+  let [dayDifference, monthDifference, yearDifference] = "";
+  yearDifference = Math.floor(dateDifference / YEARMS);
+  dateDifference = dateDifference - yearDifference * YEARMS;
+  monthDifference = Math.floor(dateDifference / MONTHMS);
+  dateDifference = dateDifference - monthDifference * MONTHMS;
+  dayDifference = Math.floor(dateDifference / DAYMS);
+
+  spanDays.innerText = dayDifference;
+  spanMonths.innerText = monthDifference;
+  spanYears.innerText = yearDifference;
+}
+
+function dateValidity() {
+  let months = [4, 6, 9, 11];
+
+  if (months.includes(parseInt(inputMonth.value))) {
+    if (inputDay.value == 31) {
+      error = 1;
+      console.log("error");
+    }
+  } else if (inputMonth.value == 2) {
+    console.log(inputYear.value);
+    let bisextile = inputYear.value / 4;
+    console.log(bisextile);
+    if (bisextile != Math.floor(bisextile) && inputDay.value > 28) {
+      error = 1;
+    } else if (inputDay.value > 29) {
+      error = 1;
+    }
+  }
+
+  if (error == 1) {
+    addErrorClass();
+    inputDay.nextElementSibling.innerText = "Must be a valid date";
   }
 }
